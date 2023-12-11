ea031d2f4158a1f27d18d507977e79bb180e3d7b
AlanEA04 <omar.encinas.cetis69@gmail.com>
2023-12-11 10:01:53 -0700

    Agregar archivos de proyecto.

---
+++ Metodos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercado
+{
+    public class Metodos
+    {
+
+        //Inserrtar un produycto
+        private Producto[] productos = new Producto[10];
+        private int cantidadProductos = 0;
+
+        public void InsertarProducto()
+        {
+            if (cantidadProductos < productos.Length)
+            {
+                Console.Write("Nombre del producto");
+                string nombre = Console.ReadLine();
+
+                Console.Write("Precio del producto");
+                double precio = double.Parse(Console.ReadLine());
+
+                productos[cantidadProductos++] = new Producto { Nombre = nombre, Precio = precio };
+            }
+            else
+            {
+                Console.WriteLine("Arreglo lleno");
+            }
+        }
+
+        //Mostrar productos
+        public void MostrarProducto()
+        {
+            if (cantidadProductos>0)
+            {
+
+            }
+
+
+
+
+
+
+        }
+
+        //Ordnamiento de los productos
+        //Busqueda de producto por precio
+        public void BuscarProducto()
+        {
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Mercado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos metodos= new Metodos();
+
+
+            //string respuesta1;
+            string respuesta1 = Console.ReadLine();
+
+            Console.WriteLine("Quieres ingresar un producto si o no");
+            respuesta1 = Console.ReadLine();
+            
+
+            if (respuesta1 == si)
+            {
+                
+                Console.Write("Nombre del producto");
+                string nombre = Console.ReadLine();
+
+                Console.Write("Precio del producto");
+                double precio = double.Parse(Console.ReadLine());
+            }
+            else
+            {
+                metodos.MostrarProducto();
+            }
+
+
+
+            //Console.WriteLine("Quieres ingresar otro producto si o no");
+            //respuesta1 = Console.ReadLine();
+
+
+            //if (respuesta1 == si)
+            //{
+            //    Console.Write("Nombre del producto");
+            //    string nombre = Console.ReadLine();
+
+            //    Console.Write("Precio del producto");
+            //    double precio = double.Parse(Console.ReadLine());
+            //}
+            //else
+            //{
+            //    metodos.MostrarProducto();
+            //}
+
+
+
+            //Console.WriteLine("Quieres buscar un producto si o no");
+            //respuesta1 = Console.ReadLine();
+
+
+            //if (respuesta1 == si)
+            //{
+
+            //    Console.Write("Precio del producto");
+            //    double precio = double.Parse(Console.ReadLine());
+
+            //}
+            //else
+            //{
+            //    metodos.MostrarProducto();
+            //}
+
+
+
+
+            //Console.WriteLine("Quieres ver la lista de los productos");
+            //respuesta1 = Console.ReadLine();
+
+
+            //if (respuesta1 == si)
+            //{
+            //    metodos.MostrarProducto();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Idk no hay productos");
+            //}
+
+        }
+    }
+}
