716e0c68f961cb917b1061883db4f73a62ae578c
Perla Ramírez Ibarra <92342192+Perla118@users.noreply.github.com>
2023-12-11 14:12:52 -0700

    actuali.cs

--- Metodos.cs
+++ Metodos.cs
@@ -1,85 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Mercado
 {
-    internal class Program
+    public class Metodos
     {
-        static void Main(string[] args)
+
+        private const int tamañoMercado = 5;
+        private static Productos[] inventario = new Productos[tamañoMercado];
+
+        private static int cantidadProductos = 0;
+
+        // Para insertar un producto en el arreglo
+        public static void InsertarProducto(string nombre, double precio)
         {
-            Metodos metodos= new Metodos();
-
-            string si = "si";
-            //string respuesta1;
-            string respuesta1 = Console.ReadLine();
-//Uncoment
-            Console.WriteLine("Quieres ingresar un producto si o no");
-            respuesta1 = Console.ReadLine();
-            
-
-            if (respuesta1 == si)
+            if (cantidadProductos < inventario.Length)
             {
-                
-                Console.Write("Nombre del producto");
-                string nombre = Console.ReadLine();
-
-                Console.Write("Precio del producto");
-                double precio = double.Parse(Console.ReadLine());
+                inventario[cantidadProductos] = new Productos { Nombre = nombre, Precio = precio };
+                cantidadProductos++;
+                Console.WriteLine("Producto");
             }
             else
             {
-                metodos.MostrarProductos();
+                Console.WriteLine("Arreglo lleno");
             }
+        }
 
-
-
-            Console.WriteLine("Quieres ingresar otro producto si o no");
-            respuesta1 = Console.ReadLine();
-
-
-            if (respuesta1 == si)
+        // productos del arreglo
+        public static void MostrarProductos()
+        {
+            if (cantidadProductos == 0)
             {
-               Console.Write("Nombre del producto");
-                string nombre = Console.ReadLine();
-
-                Console.Write("Precio del producto");
-                double precio = double.Parse(Console.ReadLine());
+                Console.WriteLine("No hay productos");
+                return;
             }
-            else
+
+            Console.WriteLine("Productos:");
+            for (int i = 0; i < cantidadProductos; i++)
             {
-                metodos.MostrarProductos();
+                Console.WriteLine($"Producto {i + 1} {inventario[i].Nombre} Precio {inventario[i].Precio}");
             }
+        }
 
-
-
-            //Console.WriteLine("Quieres buscar un producto si o no");
-            //respuesta1 = Console.ReadLine();
-
-
-            //if (respuesta1 == si)
-            //{
-
-            //    Console.Write("Precio del producto");
-            //    double precio = double.Parse(Console.ReadLine());
-
-            //}
-            //else
-            //{
-            //    metodos.MostrarProductos();
-            //}
+        //Ordenar inventaurio
 
 
 
 
-            Console.WriteLine("Quieres ver la lista de los productos si o no");
-            respuesta1 = Console.ReadLine();
 
-            if (respuesta1 == si)
+
+
+
+        // Busqnda por precio 
+        public static void BuscarPorPrecio(double precioaBuscar)
+        {
+            bool encontrado = false;
+
+            Console.WriteLine($"Productos buscados {precioaBuscar}:");
+
+            for (int i = 0; i < cantidadProductos; i++)
             {
-                metodos.MostrarProductos();
-            }
-            else
-            {
-                Console.WriteLine("No se eligió ver la lista de productos.");
+                if (Math.Abs(inventario[i].Precio - precioaBuscar) < 0.0001)
+                {
+                    Console.WriteLine($"Producto {inventario[i].Nombre} Precio {inventario[i].Precio}");
+                    encontrado = true;
+                }
             }
 
+            if (!encontrado)
+            {
+                Console.WriteLine("No hay");
+            }
         }
     }
+
+
 }
