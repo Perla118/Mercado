f9b668bc6d8a42eca20bd93b46a652fc4aa85274
Perla Ramírez Ibarra <92342192+Perla118@users.noreply.github.com>
2023-12-11 10:23:42 -0700

    adv.cs

--- Metodos.cs
+++ Metodos.cs
@@ -1,67 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Mercado
 {
-    public class Metodos
+    internal class Program
     {
-
-        //Inserrtar un produycto
-        private Producto[] productos = new Producto[10];
-        private int cantidadProductos = 0;
-
-        public void InsertarProducto()
+        static void Main(string[] args)
         {
-            if (cantidadProductos < productos.Length)
+            Metodos metodos= new Metodos();
+
+            string si = "si";
+            //string respuesta1;
+            string respuesta1 = Console.ReadLine();
+
+            Console.WriteLine("Quieres ingresar un producto si o no");
+            respuesta1 = Console.ReadLine();
+            
+
+            if (respuesta1 == si)
             {
+                
                 Console.Write("Nombre del producto");
                 string nombre = Console.ReadLine();
 
                 Console.Write("Precio del producto");
                 double precio = double.Parse(Console.ReadLine());
-
-                productos[cantidadProductos++] = new Producto { Nombre = nombre, Precio = precio };
             }
             else
             {
-                Console.WriteLine("Arreglo lleno");
+                metodos.MostrarProductos();
             }
-        }
 
-        //Mostrar productos
-        public void MostrarProducto()
-        {
-            if (cantidadProductos>0)
+
+
+            Console.WriteLine("Quieres ingresar otro producto si o no");
+            respuesta1 = Console.ReadLine();
+
+
+            if (respuesta1 == si)
             {
+               Console.Write("Nombre del producto");
+                string nombre = Console.ReadLine();
 
+                Console.Write("Precio del producto");
+                double precio = double.Parse(Console.ReadLine());
+            }
+            else
+            {
+                metodos.MostrarProductos();
             }
 
 
 
+            //Console.WriteLine("Quieres buscar un producto si o no");
+            //respuesta1 = Console.ReadLine();
 
 
+            //if (respuesta1 == si)
+            //{
+
+            //    Console.Write("Precio del producto");
+            //    double precio = double.Parse(Console.ReadLine());
+
+            //}
+            //else
+            //{
+            //    metodos.MostrarProductos();
+            //}
+
+
+
+
+            Console.WriteLine("Quieres ver la lista de los productos si o no");
+            respuesta1 = Console.ReadLine();
+
+            if (respuesta1 == si)
+            {
+                metodos.MostrarProductos();
+            }
+            else
+            {
+                Console.WriteLine("No se eligió ver la lista de productos.");
+            }
 
         }
-
-        //Ordnamiento de los productos
-        //Busqueda de producto por precio
-        public void BuscarProducto()
-        {
-
-
-
-
-
-
-
-
-        }
-
-
     }
-
 }
-
